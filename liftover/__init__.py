74e1344a3a920b2e6164e922b8bdac23b82f083c
Jeremy McRae <jmcrae@illumina.com>
2020-10-26 12:29:28 -0700

    fix py2 name conflict

--- liftover/__init__.py
+++ liftover/__init__.py
@@ -3,5 +3,5 @@ from pkg_resources import get_distribution
 __name__ = 'liftover'
 __version__ = get_distribution(__name__).version
 
-from liftover.liftover import get_lifter
+from liftover.lifter import get_lifter
 from liftover.chain_file import ChainFile
