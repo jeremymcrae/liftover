8b6e994f75a7bfeca7eea5468367c1572b5edb0e
Jeremy McRae <jeremy.mcrae@gmail.com>
2022-01-25 15:35:15 -0800

    fix build error on windows, allow linking zlib on windows

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 
 import io
-from setuptools import setup
+import glob
+import os
 import sys
-from distutils.core import Extension
+
+from distutils.ccompiler import new_compiler
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 EXTRA_COMPILE_ARGS = ['-std=c++11']
@@ -11,20 +14,44 @@ if sys.platform == "darwin":
     EXTRA_COMPILE_ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
     EXTRA_LINK_ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
 
+def build_zlib():
+    ''' compile zlib code to object files for linking with liftover on windows
+    
+    Returns:
+        list of paths to compiled object code
+    '''
+    include_dirs = ['src/zlib/']
+    sources = list(glob.glob('src/zlib/*.c'))
+    extra_compile_args = []
+    
+    cc = new_compiler()
+    return cc.compile(sources, include_dirs=include_dirs,
+        extra_preargs=extra_compile_args)
+
+gzstream_path = 'src/gzstream/gzstream.C'
+if sys.platform == 'win32':
+    zlib, libs = build_zlib(), []
+    gzstream_win_path = gzstream_path.replace('.C', '.cpp')
+    os.rename(gzstream_path, gzstream_win_path)
+    gzstream_path = gzstream_win_path
+else:
+    zlib, libs = [], ['z']
+
 lifter = cythonize([
     Extension('liftover.chain_file',
               extra_compile_args=EXTRA_COMPILE_ARGS,
               extra_link_args=EXTRA_LINK_ARGS,
               sources=['liftover/chain_file.pyx',
-                       'src/gzstream/gzstream.C',
+                        gzstream_path,
                        'src/chain.cpp',
                        'src/utils.cpp',
                        'src/headers.cpp',
                        'src/target.cpp',
                        'src/chain_file.cpp'],
-              include_dirs=['src/', 'src/gzstream/', 'src/intervaltree/'],
+              extra_objects=zlib,
+              include_dirs=['src/', 'src/gzstream/', 'src/intervaltree/', 'src/zlib/'],
               library_dirs=['src/', 'src/gzstream/', 'src/intervaltree/'],
-              libraries=['z'],
+              libraries=libs,
               language='c++'),
     ])
 
