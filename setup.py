f889a5fa26aa5d43ade4480f145644c9f1a9b2a6
Jeremy McRae <jmcrae@illumina.com>
2022-10-04 16:46:35 -0700

    fixes for macosx compilation errors

--- setup.py
+++ setup.py
@@ -51,8 +51,30 @@ def get_gzstream_path():
         gzstream_path = gzstream_win_path
     return gzstream_path
 
+def scrub_gzstream():
+    ''' workaround for compilation error on macos
+    
+    compiling gzstream requires the corresponding gzstream.h file, but if we 
+    include the gzstream directory in the include dirs, then clang complains
+    about the version file in the gzstream folder. If we remove the gzstream
+    directory from the include dirs, then clang complains about the missing
+    gzstream.h. This is because gzstream.C identifies it's header file with
+    angle brackets. Replacing the angle brackets in that line seems to work.
+    '''
+    with open(get_gzstream_path(), 'rt') as handle:
+        lines = handle.readlines()
+    
+    with open(get_gzstream_path(), 'wt') as handle:
+        for line in lines:
+            if line == '#include <gzstream.h>\n':
+                line = '#include "gzstream.h"\n'
+            handle.write(line)
+
 if sys.platform == 'win32':
     zlib, libs = build_zlib(), []
+elif sys.platform == 'darwin':
+    scrub_gzstream()
+    zlib, libs = [], ['z']
 else:
     zlib, libs = [], ['z']
 
@@ -68,8 +90,8 @@ lifter = cythonize([
                        'src/target.cpp',
                        'src/chain_file.cpp'],
               extra_objects=zlib,
-              include_dirs=['src/', 'src/gzstream/', 'src/intervaltree/', 'src/zlib/'],
-              library_dirs=['src/', 'src/gzstream/', 'src/intervaltree/'],
+              include_dirs=['src/', 'src/intervaltree/', 'src/zlib/'],
+              library_dirs=['src/', 'src/intervaltree/'],
               libraries=libs,
               language='c++'),
     ])
