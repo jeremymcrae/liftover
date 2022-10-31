c8471a5eb88305844315d5070f8e793988d6d209
Jeremy McRae <jmcrae@illumina.com>
2022-10-31 10:46:35 -0700

    use cibiuild wheel for building and testing
     - placed liftover python code inside src directory
     - fixed macosx compilation errors
    
    commit e951b22e28f0476320dd9bb5d9b618fb81dbb735
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 18:58:06 2022 -0700
    
        restore previous state
    
    commit b10226ec67607b3587c5698b1498cf17db463060
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 16:51:19 2022 -0700
    
        further compilation fixed
    
    commit f889a5fa26aa5d43ade4480f145644c9f1a9b2a6
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 16:46:35 2022 -0700
    
        fixes for macosx compilation errors
    
    commit 1d204e8e840e530f85947c4e8c276ecccb9c2c56
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 16:16:41 2022 -0700
    
        added build matrix back in
    
    commit a666401edb9a58545011e780934f2567581eb32a
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 16:16:12 2022 -0700
    
        updated macosx compilation instructions
    
    commit cb7a59e48e24e41a1e0a73c0076f8361ec6965a4
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 16:12:13 2022 -0700
    
        shifted code into subfolder to avoid testing issues
    
    commit 1b4762d9548a0f676b792844aa118a2733a14ea7
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 16:01:28 2022 -0700
    
        fix bug
    
    commit f5dc37eafd57d84845aea9c4046f0def1a5e65cf
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:59:11 2022 -0700
    
        tweak setup.py to try fixing CI error
    
    commit c4a0865f1e9df220c1194be410cbb76c848a1d5c
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:51:51 2022 -0700
    
        fix updated import
    
    commit 88a73b5ae0e648a368751c924e1583584195fa3d
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:47:28 2022 -0700
    
        check conflicting filenames
    
    commit a9b4d2621f57ff48c7116e9cbec4919f739399da
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:40:35 2022 -0700
    
        tweak setup.py to try fixing CI error
    
    commit a425d209e348184d88e7ad679e329a0600d22191
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:36:36 2022 -0700
    
        state license type in setup.py
    
    commit 077ebbdfbbc50b59b33ec300c2a44db445af53a1
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:33:51 2022 -0700
    
        try fixing ci error
    
    commit f8a1e453db26ab39fbb713c2e09be4385ff59f35
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:30:49 2022 -0700
    
        Revert "try fixing ci error"
    
        This reverts commit 715d56c3ee5f8957dfe3d2fb0a6345cfa11580fc.
    
    commit 715d56c3ee5f8957dfe3d2fb0a6345cfa11580fc
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:26:38 2022 -0700
    
        try fixing ci error
    
    commit 1b136cb44b90a794a795eaf09331fc0eb8c0afab
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:22:18 2022 -0700
    
        limit ci testing for now
    
    commit f52109d53df78c3675c9c5008f6906aebc4a03e5
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:21:59 2022 -0700
    
        try fixing ci test error
    
    commit b7c39b2c4762d42791235eb6001c98db094420b8
    Author: Jeremy McRae <jmcrae@illumina.com>
    Date:   Tue Oct 4 15:12:54 2022 -0700
    
        trying cibuildwheel to build and test

--- setup.py
+++ setup.py
@@ -11,8 +11,14 @@ from Cython.Build import cythonize
 EXTRA_COMPILE_ARGS = ['-std=c++11']
 EXTRA_LINK_ARGS = []
 if sys.platform == "darwin":
-    EXTRA_COMPILE_ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
-    EXTRA_LINK_ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
+    EXTRA_COMPILE_ARGS += [
+        "-stdlib=libc++",
+        "-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1",
+        "-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include",
+        ]
+    EXTRA_LINK_ARGS += [
+        "-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib",
+        ]
 
 def build_zlib():
     ''' compile zlib code to object files for linking with liftover on windows
@@ -44,16 +50,37 @@ def get_gzstream_path():
         gzstream_path = gzstream_win_path
     return gzstream_path
 
+def scrub_gzstream():
+    ''' workaround for compilation error on macos
+    
+    compiling gzstream requires the corresponding gzstream.h file, but if we 
+    include the gzstream directory in the include dirs, then clang complains
+    about the version file in the gzstream folder. If we remove the gzstream
+    directory from the include dirs, then clang complains about the missing
+    gzstream.h. This is because gzstream.C identifies it's header file with
+    angle brackets. Replacing the angle brackets in that line seems to work.
+    '''
+    with open(get_gzstream_path(), 'rt') as handle:
+        lines = handle.readlines()
+    
+    with open(get_gzstream_path(), 'wt') as handle:
+        for line in lines:
+            if line == '#include <gzstream.h>\n':
+                line = '#include "gzstream.h"\n'
+            handle.write(line)
+
 if sys.platform == 'win32':
     zlib, libs = build_zlib(), []
 else:
     zlib, libs = [], ['z']
 
+scrub_gzstream()
+
 lifter = cythonize([
     Extension('liftover.chain_file',
               extra_compile_args=EXTRA_COMPILE_ARGS,
               extra_link_args=EXTRA_LINK_ARGS,
-              sources=['liftover/chain_file.pyx',
+              sources=['src/liftover/chain_file.pyx',
                         get_gzstream_path(),
                        'src/chain.cpp',
                        'src/utils.cpp',
@@ -61,8 +88,8 @@ lifter = cythonize([
                        'src/target.cpp',
                        'src/chain_file.cpp'],
               extra_objects=zlib,
-              include_dirs=['src/', 'src/gzstream/', 'src/intervaltree/', 'src/zlib/'],
-              library_dirs=['src/', 'src/gzstream/', 'src/intervaltree/'],
+              include_dirs=['src/', 'src/intervaltree/', 'src/zlib/'],
+              library_dirs=['src/', 'src/intervaltree/'],
               libraries=libs,
               language='c++'),
     ])
@@ -74,8 +101,10 @@ setup(name='liftover',
       version='1.1.14',
       author='Jeremy McRae',
       author_email='jmcrae@illumina.com',
+      license='MIT',
       url='https://github.com/jeremymcrae/liftover',
       packages=['liftover'],
+      package_dir={'': 'src'},
       install_requires=[
           'requests',
       ],
@@ -84,5 +113,5 @@ setup(name='liftover',
           'Topic :: Scientific/Engineering :: Bio-Informatics',
       ],
       ext_modules=lifter,
-      test_suite='tests',
+      test_suite='unittest:TestLoader',
       )
