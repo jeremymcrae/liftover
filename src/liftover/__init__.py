d48842e93104790bf84c99b64b0628755c0be07c
Jeremy McRae <jmcrae@illumina.com>
2023-11-02 11:09:01 -0700

    avoid decrecated module import

--- src/liftover/__init__.py
+++ src/liftover/__init__.py
@@ -1,7 +1,7 @@
-from pkg_resources import get_distribution
+from importlib.metadata import version
 
 __name__ = 'liftover'
-__version__ = get_distribution(__name__).version
+__version__ = version(__name__)
 
 from liftover.lifter import get_lifter
 from liftover.chain_file import ChainFile
