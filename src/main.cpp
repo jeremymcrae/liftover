82ad4a9919716eb2b477f073a7e5ae9a221f2d40
Jeremy McRae <jmcrae@illumina.com>
2024-03-08 12:36:36 -0800

    added compilation arguments in example main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 /* example code for loading liftover chains and searching a given position
 */
 
+#include <iostream>
+#include <cstdint>
+
 #include "chain_file.h"
 
 int main() {
@@ -10,9 +13,15 @@ int main() {
   
   // search for a given coordinate
   std::string chrom = "chr1";
-  long pos = 10000000;
+  std::int64_t pos = 10000000;
   for (auto x : targets[chrom][pos]) {
     std::cout << x.contig << " pos: " << x.pos << ", on fwd:"
               << x.fwd_strand << std::endl;
   }
 }
+
+// set LD_FLAGS -fsanitize=undefined,address
+// g++ -Weverything -Wno-padded -Wno-c++98-compat -stdlib=libc++ -std=c++11  \
+//      -fsanitize=undefined,address \
+//      -lz \
+//      main.cpp chain_file.cpp chain.cpp headers.cpp target.cpp utils.cpp gzstream/gzstream.C
